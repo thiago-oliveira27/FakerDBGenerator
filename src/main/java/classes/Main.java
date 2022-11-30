c1f88501775241dc0599fa857b204751313c7184
Thiago Oliveira <thiagoolcarv27@gmail.com>
2022-11-30 16:14:33 -0300

    New looping structure to print data

--- src/main/java/classes/Main.java
+++ src/main/java/classes/Main.java
@@ -2,6 +2,9 @@ package classes;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import com.github.javafaker.Faker;
 
 public class Main {
@@ -9,10 +12,11 @@ public class Main {
     	
         Faker faker = new Faker();        
         SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
-        ArrayList<Person> person = new ArrayList<Person>();
-            
+        ArrayList<Person> person = new ArrayList<>();
+        
+        String underscore = "__________________________________";
         for(int i =0;i<5;i++) {
-        	 person.add(new Person());
+        	 person.add(new Person());       	 
         }
         
         //Setting random data to each person in Person array
@@ -25,13 +29,23 @@ public class Main {
         	 person.get(i).setCountry(faker.country().name());
         	 person.get(i).setFavoriteColor(faker.color().name());        	 
         }
+        	
+        // Printing person data without enumerating each one
+        // Using forEach structure to simplify a loop
         
+        person.forEach( c -> System.out.println(c + underscore + "\n"));
+        	
+        // Printing while enumerating person
+        
+        /*
         int n=1;
         for(int i=0;i<person.size();i++) {           	
-        	System.out.printf("\n*Person %d*\n%s",n, person.get(i)); 
-        	System.out.println("___________________________________________________________");
-        	n++;
-       }     
+           System.out.printf("\n*Person %d*\n%s",n, person.get(i)); 
+           System.out.println(underscore);
+           n++;
+        } 
+   	 	*/
+        
         System.exit(0);      
     }
 }
